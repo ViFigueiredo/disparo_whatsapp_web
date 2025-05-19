b22c2d1ded7fa0a4ad7081a1e9354c126891cbb4
Vi Figueiredo <vifigueiredo@outlook.com.br>
2025-05-19 11:54:35 -0300

    refactor(template): remove unused BusinessTemplateSelect component and clean up TemplateForm styles
    This commit deletes the BusinessTemplateSelect component, which was no longer in use, and cleans up the styles in TemplateForm.vue by removing commented-out CSS for a loading spinner. These changes aim to streamline the codebase and improve maintainability.

--- src/components/templates/TemplateForm.vue
+++ src/components/templates/TemplateForm.vue
@@ -499,27 +499,6 @@ const isCloning = computed(() => {
 </script>
 
 <style>
-/* Estilo para o spinner de loading */
-/* .spinner-border {
-  display: inline-block;
-  width: 2rem;
-  height: 2rem;
-  vertical-align: text-bottom;
-  border: 0.25em solid currentColor;
-  border-right-color: transparent;
-  border-radius: 50%;
-  -webkit-animation: spinner-border 0.75s linear infinite;
-  animation: spinner-border 0.75s linear infinite;
-}
-
-@-webkit-keyframes spinner-border {
-  to { -webkit-transform: rotate(360deg); transform: rotate(360deg); }
-}
-
-@keyframes spinner-border {
-  to { transform: rotate(360deg); }
-} */
-
 .p-dropdown {
   width: 100%;
   border-radius: 0.375rem;
